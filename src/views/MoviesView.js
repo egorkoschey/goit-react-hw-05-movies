52ada37883d799c909408b14a80f083661c24ab9
egorkoschey <egor.koschey@gmail.com>
2023-06-20 13:14:20 +0300

    init

--- src/views/MoviesView.js
+++ src/views/MoviesView.js
@@ -9,34 +9,34 @@ import { QueryContext } from '../Components/App';
 
 const MoviesView = () => {
   const [movies, setMovies] = useState([]);
-
   const { query, setQuery } = useContext(QueryContext);
   const { search, pathname } = useLocation();
   const navigate = useNavigate();
 
   useEffect(() => {
-    if (search.includes('query')) {
-      setQuery(qs.parse(search.replace('?', '')).query);
-      return;
-    }
-    setQuery('');
-  }, [search]);
+  if (search.includes('query')) {
+    setQuery(qs.parse(search.replace('?', '')).query);
+    return;
+  }
+  setQuery('');
+}, [search, setQuery]);
 
-  useEffect(() => {
-    let cancelled = false;
+useEffect(() => {
+  let cancelled = false;
 
-    !query && setMovies([]);
+  !query && setMovies([]);
 
-    if (query) {
-      navigate(`${pathname}?query=${query}`);
-      getMoviesByQuery(query)
-        .then(data => !cancelled && setMovies(data))
-        .catch(() => Notify.failure('Something went wrong!'));
-    }
-    return () => {
-      cancelled = true;
-    };
-  }, [query]);
+  if (query) {
+    navigate(`${pathname}?query=${query}`);
+    getMoviesByQuery(query)
+      .then(data => !cancelled && setMovies(data))
+      .catch(() => Notify.failure('Something went wrong!'));
+  }
+
+  return () => {
+    cancelled = true;
+  };
+}, [query, navigate, pathname, setMovies]);
 
   const onSubmit = query => setQuery(query);
 
