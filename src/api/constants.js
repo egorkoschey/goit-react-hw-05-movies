52b56741727902d3da8f5863691724ba624f9ec5
egorkoschey <egor.koschey@gmail.com>
2023-06-20 12:40:36 +0300

    init

---
+++ src/api/constants.js
@@ -0,0 +1,2 @@
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_KEY = '69ec84cfd3d16e3fd5d2c92a4c37bd55';
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import 'modern-normalize/modern-normalize.css';
+import './index.css';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
-import './index.css';
+import App from './Components/App';
+import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
